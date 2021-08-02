ce344232d5cb0cfb4543695beccb97bd9696a3a0
Clara Cook <claracook99@gmail.com>
2021-08-02 18:17:30 -0400

    added path configuration in databroker_cat

--- databroker_ls/databroker_cat/cat.py
+++ databroker_ls/databroker_cat/cat.py
@@ -1,7 +1,13 @@
 from databroker_ls.command_line import get_current_catalog
 from databroker import catalog
 
-file = "../conf_catalog.yml"
+import databroker_ls
+from os import path
+
+path_prefix = path.split(databroker_ls.__path__[0])[0]
+file = path.join(path_prefix, "conf_catalog.yml")
+
 
 def main():
     currentCatalog = get_current_catalog(file)
+    print("hi")
